182dbbfc9c55af25cb18ab7734b2c55f31d3291e
Yanwen Cheng <yanwen1210@gmail.com>
2020-10-01 00:07:13 -0400

    Change Date to Timestamp

--- src/main/resources/db/migration/V1__create_all_shopping_tables.sql
+++ src/main/resources/db/migration/V1__create_all_shopping_tables.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS userJDBCS cascade;
+DROP TABLE IF EXISTS users cascade;
 DROP TABLE IF EXISTS roles cascade;
 DROP TABLE IF EXISTS orders cascade;
 DROP TABLE IF EXISTS brands cascade;
@@ -17,7 +17,7 @@ CREATE TABLE users (
     phone           VARCHAR(20),
     email           VARCHAR(50),
     gender          VARCHAR(10),
-    birthday        DATE,
+    birthday        TIMESTAMP ,
     address         VARCHAR(100)
 );
 
@@ -34,19 +34,19 @@ CREATE TABLE roles (
 CREATE TABLE users_role (
     user_id    bigint NOT NULL,
     role_id    bigint NOT NULL,
-    FOREIGN KEY (user_id) references userJDBCS(id),
+    FOREIGN KEY (user_id) references users(id),
     FOREIGN KEY (role_id) references roles(id)
 );
 
 CREATE TABLE orders (
     id                      BIGSERIAL NOT NULL PRIMARY KEY,
     user_id                 BIGSERIAL NOT NULL,
-    order_date              DATE ,
+    order_date              TIMESTAMP ,
     order_amount            DECIMAL(20,2),
     order_status            VARCHAR(30),
-    order_shipping_date     DATE ,
+    order_shipping_date     TIMESTAMP ,
     order_tracking_number   VARCHAR(50),
-    FOREIGN KEY (user_id) references userJDBCS(id)
+    FOREIGN KEY (user_id) references users(id)
 );
 
 CREATE TABLE categories (
