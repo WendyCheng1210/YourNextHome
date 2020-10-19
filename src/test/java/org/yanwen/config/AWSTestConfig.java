d8c4a8768549ee9b61530c4ff6a79970b459586d
Yanwen Cheng <yanwen1210@gmail.com>
2020-10-19 14:57:59 -0400

    Create AWSTestConfig.java

---
+++ src/test/java/org/yanwen/config/AWSTestConfig.java
@@ -0,0 +1,34 @@
+package org.yanwen.config;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Configuration
+@Profile("unit")
+public class AWSTestConfig {
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public AmazonS3 getAmazonS3(){
+        return mock(AmazonS3.class);
+    }
+
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public AmazonSQS getAmazonSQS(){
+        //stub getQueueUrl
+        AmazonSQS amazonSQS = mock(AmazonSQS.class);
+        GetQueueUrlResult stubResult = new GetQueueUrlResult();
+        when(amazonSQS.getQueueUrl(anyString())).thenReturn(stubResult);
+        return amazonSQS;
+    }
+}
