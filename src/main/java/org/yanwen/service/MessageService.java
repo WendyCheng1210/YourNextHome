59e05b99c3cf222b03581bd75f7983c25c922057
Yanwen Cheng <yanwen1210@gmail.com>
2020-10-20 17:24:32 -0400

    Add receiveMessage method on MessageService

--- src/main/java/org/yanwen/service/MessageService.java
+++ src/main/java/org/yanwen/service/MessageService.java
@@ -5,11 +5,16 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MessageService {
@@ -36,5 +41,25 @@ public class MessageService {
                 .withMessageBody(messageBody)
                 .withDelaySeconds(delaySec);
             sqsClient.sendMessage(sendMessageRequest);
+    }
+
+    public void receiveMessage(){
+        System.out.println("Receiving messages from MyQueue.\n");
+        final ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(getQueueUrl(queueName));
+        final List<Message> msg = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
+        for (final Message message : msg) {
+            System.out.println("Message");
+            System.out.println("  MessageId:     " + message.getMessageId());
+            System.out.println("  ReceiptHandle: " + message.getReceiptHandle());
+            System.out.println("  MD5OfBody:     " + message.getMD5OfBody());
+            System.out.println("  Body:          " + message.getBody());
+            Map<String,String> messageAttributes = message.getAttributes();
+            for (final Map.Entry<String, String> entry : messageAttributes.entrySet()) {
+                System.out.println("Attribute");
+                System.out.println("  Name:  " + entry.getKey());
+                System.out.println("  Value: " + entry.getValue());
+            }
+        }
+        System.out.println();
     }
 }
