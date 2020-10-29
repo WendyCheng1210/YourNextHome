9292e4c0f4f4ccd8183616b624d3c6579a05d978
Yanwen Cheng <yanwen1210@gmail.com>
2020-10-28 23:41:25 -0400

    Update ApplicationBootstrap.java

--- mvc/src/main/java/org/yanwen/ApplicationBootstrap.java
+++ mvc/src/main/java/org/yanwen/ApplicationBootstrap.java
@@ -3,12 +3,13 @@ package org.yanwen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"org.yanwen"})//Generate dependency
 @ServletComponentScan(basePackages = {"org.yanwen.filter"})
 @EnableCaching
-public class ApplicationBootstrap {
+public class ApplicationBootstrap extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(ApplicationBootstrap.class, args);
     }
