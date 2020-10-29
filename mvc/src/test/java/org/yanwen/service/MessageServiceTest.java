c55b28ac31116bc193dd58c942d7dcd116055c5e
Yanwen Cheng <yanwen1210@gmail.com>
2020-10-28 23:41:44 -0400

    Update MessageServiceTest.java

--- mvc/src/test/java/org/yanwen/service/MessageServiceTest.java
+++ mvc/src/test/java/org/yanwen/service/MessageServiceTest.java
@@ -1,11 +1,8 @@
 package org.yanwen.service;
 
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
